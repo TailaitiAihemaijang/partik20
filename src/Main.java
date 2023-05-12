cd30e64c4a3ae3626d7eb014237c34d9ebfb70fc
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-05-12 06:57:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int n = scanner.nextInt();
+        
+        System.out.println("4'ün kuvvetleri:");
+        for (int i = 1; Math.pow(4, i) <= n; i++) {
+            System.out.println((int)Math.pow(4, i));
+        }
+        
+        System.out.println("5'in kuvvetleri:");
+        for (int i = 1; Math.pow(5, i) <= n; i++) {
+            System.out.println((int)Math.pow(5, i));
+        }
+    }
+}
